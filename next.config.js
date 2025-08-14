c795b1a381869b33ce20ce1a6b38bd5c7e844eb4
faheemmuhammed1133 <2023.muhammedf@isu.ac.in>
2025-08-14 10:21:44 +0530

    update

--- next.config.js
+++ next.config.js
@@ -10,6 +10,18 @@ const nextConfig = {
   poweredByHeader: false,
   compress: true,
   swcMinify: true,
+  // Exclude backend and problematic directories from build traces
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'backend/**/*',
+        '**/node_modules/@swc/core-linux-x64-gnu/**/*',
+        '**/node_modules/@swc/core-linux-x64-musl/**/*',
+        '**/node_modules/@swc/core-darwin-x64/**/*',
+        '**/node_modules/@swc/core-win32-x64-msvc/**/*',
+      ],
+    },
+  },
 }
 
 module.exports = nextConfig
